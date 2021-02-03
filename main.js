fbb4c7d76c0c5a6f54cec17910a8a0075dfae31e
danidnc <danieldnc@outlook.es>
2021-02-03 18:54:03 +0100

    ejercicio 1 nivel 1

---
+++ main.js
@@ -0,0 +1,7 @@
+//EJERICIO 1
+console.log("Ejercicio 1.");
+const arrNombre = ['D', 'A', 'N', 'I', 'E', 'L']; //inicializa array con el nombre
+
+for (i = 0; i < arrNombre.length; i++) { //bucle que imprime por consola posición y valor del array
+    console.log(`Letra en posición ${i} -> ${arrNombre[i]}`)
+}
